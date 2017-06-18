89af024bb9b0b41a14351e1bede37bf89e089da3
wanted <wanted@wanteds-MacBook.local>
2017-06-18 10:47:43 +0430

    Initial Commit

---
+++ Touran/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Touran
+//
+//  Created by wanted on 6/18/17.
+//  Copyright © 2017 wanted. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
